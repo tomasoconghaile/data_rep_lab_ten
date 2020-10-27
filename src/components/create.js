1ea7b746313be197367d209e4203908a2abf3e21
Krystian <kopryszek@gmail.com>
2020-10-27 15:00:33 +0000

    lab complete

--- src/components/create.js
+++ src/components/create.js
@@ -1,12 +1,93 @@
 import { render } from '@testing-library/react';
 import React from 'react';
 
-export class Create extends React.Component{
+export class Create extends React.Component {
 
-    render(){
-        return(
-            <div>
-                <h1>This is my create component</h1>
+    constructor() {
+        super();
+
+        // binding the events
+        // onSubmit - is the method that gets called when the button on our form is clicked 
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onChangeTitle = this.onChangeTitle.bind(this);
+        this.onChangeYear = this.onChangeYear.bind(this);
+        this.onChangePoster = this.onChangePoster.bind(this);
+
+        this.state = {
+            Title: '',
+            Year: '',
+            Poster: ''
+        }
+    }
+
+    // gets called when the value of our input control changes
+    // when the value changes it will update this state
+    onChangeTitle(e) {
+        this.setState({
+            Title: e.target.value
+        });
+    }
+
+    // onChangeYear method 
+    // e is the event coming back when it's invoked 
+    // setState - updating our state or object for storing
+    onChangeYear(e){
+        this.setState({
+            Year: e.target.value
+        });
+    }
+
+    // onChangePoster method 
+    onChangePoster(e){
+        this.setState({
+            Poster: e.target.value
+        });
+    }
+
+    // Method onSubmit, takes an argument 'e'
+    // e.prevenet.Default() will stop us from calling this button multiple of times
+    onSubmit(e) {
+        e.preventDefault();
+        alert("Movie: " + this.state.Title + " " + this.state.Year + " " + this.state.Poster);
+    }
+
+    // onSubmit - when the button is clicked it gonna invoke a method thisonSubmit
+    // first div is for the input control
+    // the buttom one is for the submit button
+    render() {
+        return (
+            <div className='App'>
+                <form onSubmit={this.onSubmit}>
+                    <div className='form-group'>
+                        <label>Add Movie Title: </label>
+                        <input type='text'
+                            className='form-control'
+                            value={this.state.Title}
+                            onChange={this.onChangeTitle}></input>
+                    </div>
+                    <div className='form-group'>
+                        <label>Add Movie Year: </label>
+                        <input type='text'
+                        className='form-control'
+                        value={this.state.Year}
+                        onChange={this.onChangeYear}></input>
+                    </div>
+                    <div className='form-group'>
+                        <label>Movie Poster: </label>
+                        <textarea type='text'
+                        className='form-control'
+                        value={this.state.Poster}
+                        onChange={this.onChangePoster}>
+                        </textarea>
+                    </div>
+
+
+                    <div className='form-group'>
+                        <input type='submit'
+                            value='Add Movie'
+                            className='btn btn-primary'></input>
+                    </div>
+                </form>
             </div>
         );
     }
--- src/components/read.js
+++ src/components/read.js
@@ -1,6 +1,7 @@
-//importing React
+// importing React
 import React from 'react';
 import { Movies } from './movies';
+import axios from 'axios';
 
 export class Read extends React.Component {
 
@@ -8,37 +9,31 @@ export class Read extends React.Component {
     // the stat has special properties 
     // object movies, it has data about movies
     state = {
-        movies: [
-            {
-            "Title": "Avengers: Infinity War",
-            "Year": "2018",
-            "imdbID": "tt4154756",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-            },
-            {
-            "Title": "Captain America: Civil War",
-            "Year": "2016",
-            "imdbID": "tt3498820",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-            },
-            {
-            "Title": "Charlie Wilson's War",
-            "Year": "2007",
-            "imdbID": "tt0472062",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMTgwMDgwMDc4MF5BMl5BanBnXkFtZTYwOTU3MDM4._V1_SX300.jpg"
-            }
-            ]
-            
+        movies: []
     };
 
-    //whatever is in in div will be displayed 
-    //render allows it to be working and successfully display the below header
-    //Passing the data from read component to the movies component by creating an object movies
-    //state has object called movies, passing movies down as part of an object called movies
-    render(){
+    // PROMISE 
+    // It gets called each time our component is mounted or becomes active in view 
+    componentDidMount() {
+        axios.get('https://jsonblob.com/api/jsonblob/520c3b5e-0312-11eb-a6af-cbf00d776032')
+            // .then() you write a function when it works and the happy path has been met 
+            // updating the state
+            // Json of response data is the data coming back as part of the response from the web server
+            .then((response) => {
+                this.setState({ movies: response.data.Search })
+            })
+            // .catch() writing function so it says OK if things don't workout
+            // * unfulfilled - for the error coming back *
+            .catch((error) => {
+                console.log(error)
+            });
+    }
+
+    // whatever is in in div will be displayed 
+    // render allows it to be working and successfully display the below header
+    // Passing the data from read component to the movies component by creating an object movies
+    // state has object called movies, passing movies down as part of an object called movies
+    render() {
         return (
             <div>
                 <h1>This is the Read component!</h1>
